a8e4358aee01315f656043c3b9f1ae2c0cc4ec03
Karen Huang <thinknfix2021@gmail.com>
2024-11-30 15:33:33 +0800

    add: task3 完成

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -79,16 +79,38 @@ insert into "CREDIT_PURCHASE" (user_id , credit_package_id, purchased_credits, p
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
 
+insert into "COACH" (user_id, experience_years) values
+((select id from "USER" where email = 'lee2000@hexschooltest.io'),2),
+((select id from "USER" where email = 'muscle@hexschooltest.io'),2),
+((select id from "USER" where email = 'starplatinum@hexschooltest.io'),2);
+
 -- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
+insert into "COACH_LINK_SKILL" (coach_id, skill_id) values
+('6e3d0ad0-c11d-4dfe-b607-fe91dfcc3a50', 'f483d6a4-aaca-4cba-a2f2-eedf88a58e53'),
+('ac9e051c-64ea-48bc-8ac2-8290e56a0cfa', 'f483d6a4-aaca-4cba-a2f2-eedf88a58e53'),
+('0efbc04f-890a-4044-b083-971bc8cef4d9', 'f483d6a4-aaca-4cba-a2f2-eedf88a58e53'),
+('ac9e051c-64ea-48bc-8ac2-8290e56a0cfa', '43e1e68a-cc5b-4825-b481-f257429295f9'),
+('0efbc04f-890a-4044-b083-971bc8cef4d9','093a2000-581c-471b-822f-f1750ba0db3f'),
+('0efbc04f-890a-4044-b083-971bc8cef4d9','6cda78e4-5435-461d-b8a7-2b6e13797c2a');
 
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
+update "COACH" 
+set experience_years = 3
+where user_id = '8fae9336-8287-47c2-b9f5-b4b3e9c830f4';
+
+update "COACH" 
+set experience_years = 5
+where user_id = 'b3b0deaa-e25d-4349-bd68-0ed956b32529';
+
 
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
+insert into "SKILL" (name) values ('空中瑜伽');
+delete from "SKILL" where id = '4853017b-b684-49f1-8b87-9173fd9f3e65';
 
 
 --  ████████  █████   █    █   █ 
